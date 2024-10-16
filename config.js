5fe5e58f9767476e522ab6273ae9cdcc6118059e
⚡𝘿 𝞢 𝞑 𝞓 𝙏 𝞢 𝙅🍷🕊️ <156593033+Debatej2299q@users.noreply.github.com>
2024-10-16 21:09:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,29 +8,35 @@ module.exports = {
     /**
      * bot details and parameters
      */
-    botname: process.env.BotName || "Qᴜᴇᴇɴ ᴀɴʏᴀ ᴠ2",
-    footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐀𝐧𝐲𝐚 𝐁𝐨𝐭",
+    botname: process.env.BotName || "ѕυвαяυ md",
+    footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 ɖɛɮãTëJ",
     prefa: process.env.Prefix || "-",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "⚡𝘿𝞢𝞑𝞓𝙏𝞢𝙅🍷",
+    ownernumber: process.env.Owner_Number || "918761888106",
+    instagramId: process.env.Insta || "NOT AVAILABLE NOW",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
-    packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
-    socialLink: process.env.Web || "https://github.com/PikaBotz",
-    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
+    author: process.env.Author || "👉🏻ɖɛɮ👈🏻",
+    packname: process.env.PackName || " ✮͢𝑩𝒃𝒚, ː͢im⚡𝘿𝞢𝞑𝞓𝙏𝞢𝙅 🌸𝒆🪡🩵
+
+˚∧＿∧ +  —̳͟͞͞💗
+(  •‿• )つ  —̳͟͞͞ 💗   —̳͟͞͞💗 +
+(つ　 <     —̳͟͞͞💗
+｜　 _つ  +  —̳͟͞͞💗  —̳͟͞͞💗 ˚
+`し´",
+    socialLink: process.env.Web || "https://github.com/Debatej2299q",
+    groupLink: process.env.GcLink || "https://chat.whatsapp.com/EDYe8Fto4PTJwRkC0ItRDF",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "rio2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    sessionId: process.env.SESSION_ID || "aHhxcTR_Queen-Anya_ycGs=",
 
     /**
      * thumbnails and medias
